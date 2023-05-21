57958b655bdd473e03855b2a0fd34ae201fc7a9d
sgc93 <smachewgedefaw@gmail.com>
2023-05-21 10:03:18 -0900

    final c-s calculator

---
+++ CS_Calculator_TCP/TCPClient.java
@@ -0,0 +1,47 @@
+package CS_Calculator_TCP;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+
+public class TCPClient {
+
+    private static final String ADDRESS = "localhost";
+    private static final int PORT = 4000;
+
+    public static void main(String[] args){
+        try (
+            Socket con = new Socket(ADDRESS, PORT);
+        ){
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
+            PrintWriter out = new PrintWriter(con.getOutputStream(), true);
+
+            String message, response;
+            do {
+                System.out.println("------------------------------------------------");
+                System.out.println("Inter your mathematical expression like 23 + 43");
+                System.out.println("Don't forget to use a space to separate each term, i.e numbers and operaor should be separated with space.");
+                System.out.println("------------------------------------------------");
+                System.out.print("Question: ");
+                message = br.readLine();
+                out.println(message);
+                if(message.equals("stop")){
+                    System.out.println("you have just exit the process!");
+                    break;
+                }
+
+                response = in.readLine();
+                System.out.println(response); // print the response red and bold
+                System.out.print("\033[0m");  // reset the font of the console
+            } while(true);
+            br.close();
+            in.close();
+            out.close();
+            con.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ CS_Calculator_TCP/TCPServer.java
@@ -0,0 +1,68 @@
+package CS_Calculator_TCP;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class TCPServer {
+    private static final int PORT = 4000;
+    public static void main(String[] args){
+        try{
+            try (ServerSocket server = new ServerSocket(PORT)) {
+                System.out.println("waiting for client ... ");
+                Socket con = server.accept();   // connecting
+                System.out.println("connection created with client!");
+                BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream())); // from the  client
+                PrintWriter out = new PrintWriter(con.getOutputStream(), true); // to the clinet 
+                // declare varivables
+                String message, response;
+                double num1, num2;
+                char operator;
+
+                do {
+                    message = in.readLine();
+                    if(message.equals("stop")){
+                        break;
+                    }
+                    String[] splited = message.split(" ");
+                    try {
+                        num1 = Double.parseDouble(splited[0]);
+                        num2 = Double.parseDouble(splited[2]);
+                        operator = splited[1].charAt(0);  
+                        switch(operator){
+                            case '+':
+                            response = Double.toString(num1 + num2);
+                            break;
+                            case '-':
+                            response = Double.toString(num1 - num2);
+                            break;
+                            case '/':
+                            response = Double.toString(num1 / num2);
+                            break;
+                            case '*':
+                            response = Double.toString(num1 * num2);
+                            break;
+                            default:
+                            response = "\033[31m" + "\033[1m" + "UNclear operator";
+                            break;
+                        }
+                        out.println("Answer: " + response);
+                    } catch(Exception e){
+                        out.println("\033[31m" + "\033[1m" + "You have sent incorrect expression!");
+                    }
+                } while(true);
+                // in.close();
+                // out.close();
+                // con.close();
+            } catch (NumberFormatException e) {
+                e.printStackTrace();
+            }
+
+        } catch(IOException e){
+            e.printStackTrace();
+        }
+    }
+}
